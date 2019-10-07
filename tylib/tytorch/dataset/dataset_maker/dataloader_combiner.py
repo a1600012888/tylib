623b2de1ecc2014a8d3a1cb1e534aa9c5c54fb51
Zhen Dong <zhen@a20.Millennium.Berkeley.EDU>
2019-10-07 08:33:50 -0700

    new loader

--- tylib/tytorch/dataset/dataset_maker/dataloader_combiner.py
+++ tylib/tytorch/dataset/dataset_maker/dataloader_combiner.py
@@ -5,22 +5,61 @@ import torch
 # all above is for dataset combiner!
 # we want data loader combiner!
 
+class _DataloaderCombiner(object):
+    '''
+    uniform sampling for each loader
+    '''
+
+    def __init__(self, loaders):
+        
+        self.loaders = []
+        for loader in loaders:
+            self.loaders.append(enumerate(loader))
+        #self.loaders = loaders
+        self.num_loader = len(loaders)
+        self.lens = [len(loader) for loader in loaders]
+        total_len = 0 
+        for l in self.lens:
+            total_len += l
+        self.total_len = total_len
+
+
+    def __next__(self):
+
+        idx = int(torch.randint(low=0, high=self.num_loader, size=(1,))[0])
+        loader = self.loaders[idx]
+
+        i, batch = next(loader)
+        #print(i)
+        #print(self.lens)
+        
+        if i == self.lens[idx]-1:
+            self.loader.pop(idx)
+            self.num_loader -= 1
+            self.lens.pop(idx)
+        return batch
+
+    def __len__(self):
+        return self.total_len
+
 class DataloaderCombiner(object):
     '''
     uniform sampling for each loader
     '''
 
     def __init__(self, loaders):
-
-        self.loaders = loaders
+        
         self.num_loader = len(loaders)
-
+        self.lens = [len(loader) for loader in loaders]
+        total_len = 0 
+        for l in self.lens:
+            total_len += l
+        self.total_len = total_len
+        self.dataloader_combiner = _DataloaderCombiner(loaders)
 
     def __iter__(self):
+        return self.dataloader_combiner
 
-        idx = int(torch.randint(0, self.num_loader, size=(1))[0])
-        loader = self.loaders[idx]
+    def __len__(self):
+        return self.total_len
 
-        batch = next(loader)
-
-        yield batch
